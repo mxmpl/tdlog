138ff28a1b6ca341a1dcbd1e8aaed7b9f080248e
Margot Cosson <margotcosson1997@gmail.com>
2019-11-15 16:42:45 +0100

    creation app main

--- prototype/exemple.py
+++ prototype/exemple.py
@@ -1,1 +1,18 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/', methods=['POST'])
+def text_box():
+    text = request.form['text']
+    processed_text = text.upper()
+    return render_template("bienvenue.html" , message = processed_text )
+
+if __name__ == '__main__':
+    app.run()
