4e75efc446d62621359b4cf386dbb8f7663eabfa
Raphael LASRY <raphaellasry1@hotmail.fr>
2019-11-15 17:08:57 +0100

    Changements : écriture dans le csv mais probleme de délétion de ce qui est déja ecrit dedans

--- prototype/exemple.py
+++ prototype/exemple.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import csv 
+
 
 app = Flask(__name__)
 
@@ -11,6 +13,10 @@ def home():
 @app.route('/', methods=['POST'])
 def text_box():
     text = request.form['text']
+    
+    with open("chantiers.csv", mode = "w") as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=",")
+        csv_writer.writerow([text])
     processed_text = text.upper()
     return render_template("bienvenue.html" , message = processed_text )
 
