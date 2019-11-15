8b1866cb8f23a5abee26a4befac0f2f26cb95603
Maxime Poli <maxime.poli@eleves.enpc.fr>
2019-11-15 21:26:06 +0100

    ajout chantier, choix employe

--- prototype/exemple.py
+++ prototype/exemple.py
@@ -1,28 +1,24 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-# import pandas
+import pandas
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def text_box():
-    text = request.form['text']
+    text = request.form["text"]
     
-    with open('chantiers.csv','a') as open_csv:
-    	open_csv.write(text)
-   	# Fonctionne pas mais pourra servir dans un deuxieme temps
-    # df = pandas.read_csv('chantiers.csv')
-    # df.append([text])
-    # df.to_csv("chantiers.csv", sep=',', encoding='utf-8')
+    with open("chantiers.csv","a") as open_csv:
+        open_csv.write("\n"+text)
+    df = pandas.read_csv("employes.csv")
     processed_text = text.upper()
-    return render_template("bienvenue.html" , message = processed_text )
+    return render_template("bienvenue.html" , message = processed_text, employe = df["Nom"][0])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
-
