27d76eb4ca0f467725a5229478d64559d41d5880
Maxime Poli <maxime.poli@eleves.enpc.fr>
2019-11-18 10:17:36 +0100

    reset button

--- prototype/exemple.py
+++ prototype/exemple.py
@@ -22,15 +22,24 @@ def assigner_chantier_a_ouvrier():
 
 @app.route("/affichage_planning")
 def affichage_planning():
-    if os.path.exists("templates/dataframe.html"):
-        os.remove("templates/dataframe.html") 
     chantiers = pd.read_csv("chantiers.csv", index_col = "Nom", sep=",")
     html = chantiers.to_html()
+    if os.path.exists("templates/dataframe.html"):
+        print("ok")
+        os.remove("templates/dataframe.html") 
     text_file = open("templates/dataframe.html", "w")
     text_file.write(html)
     text_file.close()
     return render_template("dataframe.html")
          
+@app.route("/reset")
+def reset():
+    chantiers = pd.read_csv("chantiers.csv", index_col = "Nom", sep=",")
+    for i in range(len(chantiers.loc[:,"Ouvrier"])):
+        chantiers.loc[:,"Ouvrier"][i] = " "
+    chantiers.to_csv("chantiers.csv", sep=",")
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run()
