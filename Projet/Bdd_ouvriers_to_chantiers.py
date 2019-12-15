d3abbe8ae5984475f69f942151ec19a80713b93e
Raphael LASRY <raphaellasry1@hotmail.fr>
2019-12-15 10:37:40 +0100

    Leger changements sur attribution chantiers

--- Projet/Bdd_ouvriers_to_chantiers.py
+++ Projet/Bdd_ouvriers_to_chantiers.py
@@ -1,11 +1,26 @@
+######################################################
+
+'''
+Projet TDLOG réalisé par Maxime BRISINGER, Margot COSSON, Raphael LASRY et Maxime POLI, 2019-2020
+
+Le but de ce script python est de traiter les bases de données relatives aux liens ouvriers/chantiers.
+
+'''
+
+###################################################### Import des bibliothèques utiles
+
 import sqlite3
 
-db = sqlite3.connect('liste_attribution') # La base de données listant tous les chantiers
+###################################################### Création de la base de données bdd_liste_attribution
+
+db = sqlite3.connect('bdd_liste_attribution') # La base de données listant tous les couples chantiers-ouvriers
 cursor = db.cursor() # On se place sur cette bdd
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS attribution(id INTEGER PRIMARY KEY,
                                                     id_ouvrier INTEGER, id_chantier INTEGER)''')
 db.commit() # On termine de creer la table
+
+###################################################### Ajout d'un nouveau couple à notre base de données
 
 # new_attribution = get_data() # données à récupérer depuis Python 
 new_attribution = ["1", "2"]
@@ -17,6 +32,7 @@ cursor.execute('''INSERT INTO attribution(id_ouvrier, id_chantier)
                   VALUES(?,?)''', (new_attribution[0], new_attribution[1]))
 db.commit()
 
+###################################################### Sélection de certains ouvriers particuliers
 
 def Select_condition (command: str):
     cursor.execute(command)
