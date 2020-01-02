219b321146a6828bcb8a5373a9f72fb319700495
Maxime Brisinger <maximebrisinger@gmail.com>
2020-01-02 17:16:49 +0100

    m

--- calendar-project/Flask/ChantiersService.py
+++ calendar-project/Flask/ChantiersService.py
@@ -4,21 +4,25 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-chantiers = [
-{"start":"2020-01-05", "title":"Champs-sur-Marne", "ouvrier":"Max"},
-{"start":"2020-01-08", "title":"Paris", "ouvrier":"Raph"},
-{"start":"2020-01-10", "title":"Bordeaux", "ouvrier":"Margot"},
-{"start":"2020-01-12", "title":"Noisy", "ouvrier":"Max2"}
+attribution = [
+{"start":"2020-01-05", "chantier":"Champs-sur-Marne", "ouvrier":"Max"},
+{"start":"2020-01-08", "chantier":"Paris", "ouvrier":"Raph"},
+{"start":"2020-01-10", "chantier":"Bordeaux", "ouvrier":"Margot"},
+{"start":"2020-01-08", "chantier":"Noisy", "ouvrier":"Max2"},
+{"start":"2020-01-12", "chantier":"Mulhouse", "ouvrier":"Fredo", "end":"2020-01-15"}
 ]
 
+for dico in attribution:
+    dico["title"] = dico["ouvrier"] + " est a " + dico["chantier"]
+    
 @app.route("/", methods=['GET'])
 def index():
     return "Welcome";
 
 @app.route("/listeChantiers/", methods = ['GET'])
 def ListeChantiers():
-    global chantiers
-    return jsonify(chantiers)
+    global attribution
+    return jsonify(attribution)
 
 if __name__ == '__main__':
     app.run(debug=True)
