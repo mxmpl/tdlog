2708edf812fa8a820ccd7f501c3d18d45ae396a7
Maxime Brisinger <maximebrisinger@gmail.com>
2020-01-04 21:02:48 +0100

    m

--- calendar-project/Flask/ChantiersService.py
+++ calendar-project/Flask/ChantiersService.py
@@ -5,12 +5,22 @@ app = Flask(__name__)
 CORS(app)
 
 attribution = [
+{"start":"2020-01-05", "chantier":"Champs-sur-Marn", "ouvrier":"Max"},
+{"start":"2020-01-08", "chantier":"Paris", "ouvrier":"Raph"},
+{"start":"2020-01-10", "chantier":"Bordeaux", "ouvrier":"Margot"},
+{"start":"2020-01-08", "chantier":"Noisy", "ouvrier":"Max2"},
+{"start":"2020-01-12", "chantier":"Mulhouse", "ouvrier":"Fredo", "end":"2020-01-15"}
+]
+
+test = { "totalItems":5,"items":[
 {"start":"2020-01-05", "chantier":"Champs-sur-Marne", "ouvrier":"Max"},
 {"start":"2020-01-08", "chantier":"Paris", "ouvrier":"Raph"},
 {"start":"2020-01-10", "chantier":"Bordeaux", "ouvrier":"Margot"},
 {"start":"2020-01-08", "chantier":"Noisy", "ouvrier":"Max2"},
 {"start":"2020-01-12", "chantier":"Mulhouse", "ouvrier":"Fredo", "end":"2020-01-15"}
 ]
+}
+
 
 for dico in attribution:
     dico["title"] = dico["ouvrier"] + " est a " + dico["chantier"]
@@ -18,11 +28,17 @@ for dico in attribution:
 @app.route("/", methods=['GET'])
 def index():
     return "Welcome";
-
+    
 @app.route("/listeChantiers/", methods = ['GET'])
 def ListeChantiers():
     global attribution
     return jsonify(attribution)
+    
+@app.route("/listeOuvriers/", methods = ['GET'])
+def ListeOuvriers():
+    global test
+    return jsonify(test)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
