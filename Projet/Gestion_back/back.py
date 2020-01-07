16240df25b2642a001975304fe0d843085290643
Margot Cosson <margotcosson1997@gmail.com>
2020-01-07 20:18:09 +0100

    back plus propre

--- Projet/Gestion_back/back.py
+++ Projet/Gestion_back/back.py
@@ -24,73 +24,48 @@ from Gestion_bdd import bdd
 
 ############# Creation du site
 
-APP = Flask(__name__)  # Creation du site
+#APP = Flask(__name__)  # Creation du site
 
 #%% Fonctions du back
 
-
-def set_new_attribution_name(dict_new_attributions: dict):
-    """
-    dict_new_attributions doit être un dictionnaire qui associe
-    un nom d'ouvrier à un nom de chantier.
-    Discuter du fait que si on déclare une nouvelle attribution avec les noms
-    du chantier et de l'ouvrier, cela peut etre problematique en cas de doublons
-    de noms.
-    """
-    for ouvrier in dict_new_attributions.keys():  # ouvrier est un type str
-        id_ouvrier = bdd.get_id_from_name_ouvrier(ouvrier)
-        id_chantier = bdd.get_id_from_name_chantier(dict_new_attributions[ouvrier])
-        if verif_dispo_horaire_ouvrier(id_ouvrier, id_chantier):
-            bdd.insert_attribution([id_ouvrier, id_chantier])
-
-def set_new_attribution_id(dict_new_attributions: dict): 
-    """
-    dict_new_attributions doit être un dictionnaire qui associe
-    un id d'ouvrier à un id de chantier.
-    """
-    for id_ouvrier in dict_new_attributions.keys():  # id est un type int
-        id_chantier = dict_new_attributions[id_ouvrier]
-        if verif_dispo_horaire_ouvrier(id_ouvrier, id_chantier):
-            bdd.insert_attribution([id_ouvrier, id_chantier])
-
-
+#%% SET 
 
 def set_new_chantier(dict_new_chantier: dict):
     """
-    dict_new_chantier est un dictionnaire qui associe à " "
-    le nom du nouveau chantier.
+    Permet d'inserer un nouveau chantier. Le format d'entrée
+    doit être un dictionnaire de la forme
+    {"name_chantier": text, "start": text, "end": text, "adress": text}.
     """
-    for clef in dict_new_chantier.keys():
-        new_chantier = [str(dict_new_chantier[clef]), "NULL", "NULL", "NULL"]
-        bdd.insert_chantier(new_chantier)
-
+    bdd.insert_chantier(dict_new_chantier)
+    # Gestion d'erreur à faire dessus 
 
 def set_new_ouvrier(dict_new_ouvrier: dict):
     """
-    dict_new_ouvrier est un dictionnaire qui associe à " "
-    le nom du nouvel ouvrier.
+    Permet d'inserer un nouveau ouvrier. Le format d'entrée
+    doit être un dictionnaire de la forme
+    {"name_ouvrier": text}.
     """
-    for clef in dict_new_ouvrier.keys():
-        new_ouvrier = [str(dict_new_ouvrier[clef]), "NULL", bdd.DISPONIBLE]
-        bdd.insert_ouvrier(new_ouvrier)
+    bdd.insert_ouvrier(dict_new_ouvrier)
+    # Gestion d'erreur à faire dessus 
 
+def set_new_attribution(dict_new_attribution: dict): 
+    """
+    Permet d'inserer un couple d'id_ouvrier/id_chantier.
+    Format d'entrée : dict_new_attribution = {"id_ouvrier": int, "id_chantier": int}
+    """
+    if verif_dispo_horaire_ouvrier(dict_new_attribution["id_ouvrier"], dict_new_attribution["id_chantier"]):
+        bdd.insert_attribution(dict_new_attribution)
+    else: 
+        pass #Gestion d'erreur à faire 
 
 def get_planning():
     """
-    Cette fonction permet de renvoyer une liste de listes telles que
-    [[nom_ouvrier1, nom_chantier1, date_debut, date_fin],
-    [nom_ouvrier2, nom_chantier2, date_debut, date_fin], ...]
-    Attention, si aucune information n'a été remplie, cela renvoie une liste vide
-    telle que [].
+    Renvoie toutes les attributions et les informations sur les ouvriers et
+    les chantiers correspondantes sous la forme d'une liste de dictionnaires
+    telle que [{"id_ouvrier": int, "name_ouvrier": text, "id_chantier": int,
+    "name_chantier": text, "start": text, "end": text, "adress": text},]
     """
     return bdd.get_all_attribution()
-
-def conversion_liste_dict(liste: list): 
-    """
-    Cette fonction permet de convertir une liste d'attribution [nom_ouvrier1, 
-    nom_chantier1, date_debut, date_fin] en dictionnaire 
-    """
-    pass
 
 def verif_dispo_horaire_ouvrier(id_ouvrier: int, id_chantier: int):
     """
@@ -98,17 +73,17 @@ def verif_dispo_horaire_ouvrier(id_ouvrier: int, id_chantier: int):
     l'ouvrier est disponible sur la plage horaire concernée pour ce
     chantier. Return true si l'ouvrier n'est pas encore occupé sur ce créneau.
     """
-    if bdd.get_dates_from_id_chantier(
-            id_chantier
-    ) not in bdd.get_attribution_hours_one_ouvrier(id_ouvrier):
+    planning = bdd.get_planning_individuel(id_ouvrier)
+    infos_chantier = bdd.get_info_from_id_chantier(id_chantier)
+    for chantier in planning: 
+        if(chantier["start"] == infos_chantier["start"] and chantier["end"] == infos_chantier["end"]): 
+            raise Exception("L'ouvrier {} est déjà occupé sur la période {}-{}".format(bdd.get_info_id_ouvrier(id_ouvrier)["name_ouvrier"], infos_chantier["start"], infos_chantier["end"]))
+            return False 
         return True
-    return False
 
-
-
-if __name__ == "__main__":
-    APP.debug = False  
-    APP.run()
+#if __name__ == "__main__":
+#    APP.debug = False  
+#    APP.run()
 
 # À la fin de l'utilisation, on supprime les tables
 
