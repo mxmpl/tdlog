5cccd69ec15033a4651d663cd5771effacc1bc14
Maxime Poli <maxime.poli@eleves.enpc.fr>
2020-01-10 16:48:09 +0100

    select-chantier

--- calendar-project/src/app/modules/app-routing.module.ts
+++ calendar-project/src/app/modules/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'detail/:id', component: OuvrierDetailComponent },
   { path: 'ouvriers', component: OuvriersComponent },
   { path: 'chantiers', component: ChantiersComponent},
-  { path: 'ouvriers-detail', component: OuvrierDetailComponent}
+  { path: 'ouvriers-detail', component: OuvrierDetailComponent},
 ];
 
 @NgModule({
---
+++ calendar-project/src/app/services/chantier.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Ouvrier } from '../ouvrier';
+import { Chantier } from '../chantier';
+import { MessageService } from './message.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChantierService {
+
+  private listeChantiersUrl = 'http://127.0.0.1:5000/listeOuvriers/';
+  private listeOuvriersUrl = 'http://127.0.0.1:5000/listeOuvriers/';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  getChantiersDispos(id_ouvrier: number): Observable<Map<string,any[]>> {
+    return this.http.get<Map<string,any[]>>(this.listeOuvriersUrl+'${id_ouvrier}'+'/chantiersdispos')
+      .pipe(
+        tap(_ => this.log('chantiers dispos récupérés')),
+        catchError(this.handleError<Ouvrier[]>('getChantiersDispos', []))
+      );
+  }
+
+    /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a OuvrierService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`OuvrierService: ${message}`);
+  }
+}
+
+}
