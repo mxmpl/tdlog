60323ea3d7e5ba19797551b26e1c6d75f902678c
Raphael LASRY <raphaellasry1@hotmail.fr>
2020-01-10 17:01:43 +0100

    MAJ

--- Projet/Gestion_back/exemples_back.py
+++ Projet/Gestion_back/exemples_back.py
@@ -2,3 +2,12 @@ from back import *
 
 
 #print(return_table_ouvrier_avec_chantiers())
+
+
+print(return_table("ouvriers"))
+#print(get_planning_individuel(1))
+#print(return_cluster_chantiers(1))
+
+
+modify_data("ouvriers", "name_ouvrier", "Max", id_ouv = 1)
+print(return_table("ouvriers"))
