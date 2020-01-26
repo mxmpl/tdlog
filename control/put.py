155e5fc749c4c7706bb7a81691a0f50284964890
Margot Cosson <margotcosson1997@gmail.com>
2020-01-26 17:11:19 +0100

    maj

--- control/put.py
+++ control/put.py
@@ -176,11 +176,6 @@ def declare_new_chantier(dict_new_chantier: dict):
         dict_new_chantier,
         {"name_chantier": str, "start": str, "end": str, "adress": str},
     )
-    try:
-        # On vérifie que le nom ne soit pas déjà utilisé par un autre chantier
-        bdd.name_in_table("chantiers", dict_new_chantier["name_chantier"])
-        list_new_chantiers = decoup_new_chantier(dict_new_chantier)
-        for chantier in list_new_chantiers:
-            set_new_chantier(chantier)
-    except:
-        raise ex.NameAlreadyExists(dict_new_chantier["name_chantier"])
+    list_new_chantiers = decoup_new_chantier(dict_new_chantier)
+    for chantier in list_new_chantiers:
+        set_new_chantier(chantier)
